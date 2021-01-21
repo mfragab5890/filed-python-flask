82aa8c0facaebe24b9e7d35e91c880afc050f84c
ghelpo <m.f.ragab5890@gmail.com>
2021-01-21 16:34:08 +0200

    Update make_payments.py

--- src/make_payments.py
+++ src/make_payments.py
@@ -1,7 +1,26 @@
 from .payment_gateways import ExpensivePaymentGateway, PremiumPaymentGateway, CheapPaymentGateway
+from flask import abort, jsonify
+
 cc = 5104740265868658
+
+
 def make_payments(data):
-    if 21 > data.amount > 0:
+    if 0 < data['amount'] < 21:
         try:
-            CheapPaymentGateway(data)
+            status = CheapPaymentGateway(data)
+            if status['success']:
+                return jsonify({
+                    "success": True,
+                    "message": 'Payment is processed'
+                })
         except:
+            abort(500)
+    elif 21 <= data['amount'] <= 500:
+        try:
+            ExpensivePaymentGateway(data)
+        except:
+            try:
+                CheapPaymentGateway(data)
+            except:
+                abort(500)
+    elif data['amount'] > 500:
