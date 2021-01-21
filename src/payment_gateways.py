07e9428d4cbb2875ee748b8c8eefcbb4a261db05
ghelpo <m.f.ragab5890@gmail.com>
2021-01-21 16:34:32 +0200

    Update payment_gateways.py

--- src/payment_gateways.py
+++ src/payment_gateways.py
@@ -1,4 +1,4 @@
-from flask import jsonify, abort
+from flask import jsonify
 
 from .models.models import *
 
@@ -7,11 +7,11 @@ from .models.models import *
 def PremiumPaymentGateway(data):
     try:
         payment = PremiumPayment(
-            credit_card_number=data.credit_card_number,
-            card_holder=data.card_holder,
-            expiration_date=data.expiration_date,
-            security_code=data.security_code,
-            amount=data.amount
+            credit_card_number=data['credit_card_number'],
+            card_holder=data['card_holder'],
+            expiration_date=data['expiration_date'],
+            security_code=data['security_code'],
+            amount=data['amount']
         )
         payment.insert()
         return jsonify({
@@ -29,11 +29,11 @@ def PremiumPaymentGateway(data):
 def ExpensivePaymentGateway(data):
     try:
         payment = ExpensivePayment(
-            credit_card_number=data.credit_card_number,
-            card_holder=data.card_holder,
-            expiration_date=data.expiration_date,
-            security_code=data.security_code,
-            amount=data.amount
+            credit_card_number=data[ 'credit_card_number' ],
+            card_holder=data[ 'card_holder' ],
+            expiration_date=data[ 'expiration_date' ],
+            security_code=data[ 'security_code' ],
+            amount=data[ 'amount' ]
         )
         payment.insert()
         return jsonify({
@@ -51,11 +51,11 @@ def ExpensivePaymentGateway(data):
 def CheapPaymentGateway(data):
     try:
         payment = CheapPayment(
-            credit_card_number=data.credit_card_number,
-            card_holder=data.card_holder,
-            expiration_date=data.expiration_date,
-            security_code=data.security_code,
-            amount=data.amount
+            credit_card_number=data[ 'credit_card_number' ],
+            card_holder=data[ 'card_holder' ],
+            expiration_date=data[ 'expiration_date' ],
+            security_code=data[ 'security_code' ],
+            amount=data[ 'amount' ]
         )
         payment.insert()
         return jsonify({
