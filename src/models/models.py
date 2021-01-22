2f2f0795515aa71548d75625323620f7225f2e00
ghelpo <m.f.ragab5890@gmail.com>
2021-01-22 16:15:55 +0200

    Update models.py

--- src/models/models.py
+++ src/models/models.py
@@ -2,6 +2,7 @@ import os
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_validator import ValidateCreditCard
 
 # Declare database name
 database_name = "filed"
@@ -66,6 +67,10 @@ class ExpensivePayment(db.Model):
         db.session.add(self)
         db.session.commit()
 
+    @classmethod
+    def __declare_last__(cls):
+        ValidateCreditCard(ExpensivePayment.credit_card_number, true, true, "The Credit card number is not valid. "
+                                                                            "Please check it")
 
 # Cheap Payment Gateway table
 class CheapPayment(db.Model):
